90f752eff51bed01454d4d8c29f53f12848cf0fd
Prahlad Yeri <prahladyeri@yahoo.com>
2014-07-08 18:14:48 +0530

    Fix for #1

--- setup.py
+++ setup.py
@@ -23,17 +23,27 @@ s = setup(name='hotspotd',
 	
 
 if 'install' in sys.argv:
-	loc=os.sep.join([sys.prefix, 'lib', 'python' + sys.version[:3], 'site-packages'])
+	pkk = 'site-packages'
+	#NOTE: sys.prefix doesn't equate to /usr/local for some reason
+	loc=os.sep.join(['/usr/local', 'lib', 'python' + sys.version[:3], 'site-packages'])
+	if not os.path.exists(loc+'/hotspotd'):
+		loc=os.sep.join(['/usr/local', 'lib', 'python' + sys.version[:3], 'dist-packages'])
+	
 	for i in range(len(sys.argv)):
 		s = sys.argv[i]
 		if '--prefix=' in s:
 			s=s.replace('--prefix=','').strip()
 			loc=os.sep.join([s, 'lib', 'python' + sys.version[:3], 'site-packages'])
+			if not os.path.exists(loc+'/hotspotd'):
+				loc=os.sep.join([s, 'lib', 'python' + sys.version[:3], 'dist-packages'])
 		elif s.strip()=='--prefix':
 			n=i+1
 			if n<=len(sys.argv):
 				s = sys.argv[n]
 				loc=os.sep.join([s, 'lib', 'python' + sys.version[:3], 'site-packages'])
+				if not os.path.exists(loc+'/hotspotd'):
+					loc=os.sep.join([s, 'lib', 'python' + sys.version[:3], 'dist-packages'])
+	print 'Install base: ' + loc
 
 	import distutils
 	distutils.file_util.copy_file(loc + '/hotspotd/hotspotd.py','/usr/bin/hotspotd',link='sym',preserve_mode=1)
